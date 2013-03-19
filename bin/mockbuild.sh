0bfe87d85dcca3d79a28e418f563943f98b68911
Adrien Devresse <Adev88@gmail.com>
2013-03-19 13:47:10 +0100

    - fix problem of the mock path for bamboo

--- bin/mockbuild.sh
+++ bin/mockbuild.sh
@@ -9,6 +9,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "*** begin mock build *** "
 MOCK_CONFIG=$1
+MOCK_EXE="/usr/bin/mock"
 MOCK_OPTS_CLEAN="--scrub=yum-cache"
 MOCK_CONFIG_DIRNAME="$( cd "$DIR/../mock_configs" && pwd )"
 SRC_RPM_DIR=$2
@@ -24,10 +25,10 @@ echo "## list mock config..."
 ls $MOCK_CONFIG_DIRNAME
 
 echo "## launch mock clean ..."
-mock --configdir=${MOCK_CONFIG_DIRNAME}/ -r $MOCK_CONFIG $MOCK_OPTS_CLEAN  || true
+$MOCK_EXE --configdir=${MOCK_CONFIG_DIRNAME}/ -r $MOCK_CONFIG $MOCK_OPTS_CLEAN  || true
 echo "## launch mock ..."
 echo " build command mock --configdir=${MOCK_CONFIG_DIRNAME}/ -r $MOCK_CONFIG $RPMS_NAME"
-mock --configdir=${MOCK_CONFIG_DIRNAME}/ -r $MOCK_CONFIG $RPMS_NAME
+$MOCK_EXE --configdir=${MOCK_CONFIG_DIRNAME}/ -r $MOCK_CONFIG $RPMS_NAME
 
 echo "## creat link to result "
 rm -f $DEST_RPM_DIR/
