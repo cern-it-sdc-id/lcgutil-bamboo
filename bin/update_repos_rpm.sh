c2788339312f6d1dcf8c05a223603b368d6826da
Adrien Devresse <Adev88@gmail.com>
2013-03-19 16:19:23 +0100

    - incrase verbosity of update rpm

--- bin/update_repos_rpm.sh
+++ bin/update_repos_rpm.sh
@@ -10,6 +10,7 @@ RPM_DIR=$( echo "$1" | sed 's@\([^/]\)/*@\1@g')
 REPO_ADDR=$2
 
 echo "*** test update repos ***** "
+echo "SOURCE RPM Dir : $RPM_DIR"
 echo "destination repo : $REPO_ADDR"
 
 echo "*** list RPMS ***"
@@ -18,7 +19,7 @@ ls -l $RPM_DIR/*.rpm
 echo " *** copy RPMS ***"
 cp $RPM_DIR/*.rpm $REPO_ADDR
 
-echo "*** renew repo ***"
+echo "*** renew repo $REPO_ADDR ***"
 createrepo $REPO_ADDR
 
 echo "*** clean everything ***"
