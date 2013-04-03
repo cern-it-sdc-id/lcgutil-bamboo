a6d41d4a2847e48a896b271289809ad76146c63c
Adrien Devresse <Adrien.Devresse@cern.ch>
2013-04-03 14:13:29 +0200

    - fix update repos with a resilient copyfile usage

--- bin/update_repos_rpm.sh
+++ bin/update_repos_rpm.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+
+
+MY_DIR=$(dirname $(readlink -f $0) )
+
+# import copyfile
+source $MY_DIR/copy_files.sh
+source $MY_DIR/lockers.sh
+
+function update_repo_func {
+
+echo " *** copy RPMS ***"
+copy_files "$RPM_DIR/*.rpm" "$REPO_ADDR"
+
+echo "*** renew repo $REPO_ADDR ***"
+createrepo $REPO_ADDR
+
+
+}
+
+
 set -e
 
 if [[ "$1" == "" || "$2" == "" ]]; then
@@ -6,21 +26,19 @@ if [[ "$1" == "" || "$2" == "" ]]; then
 	exit 0
 fi
 
-RPM_DIR=$( echo "$1" | sed 's@\([^/]\)/*@\1@g')
+RPM_DIR=$( echo "$1" | sed 's@//*@/@g' | sed 's@\([^/]\)/$@\1@g' )
 REPO_ADDR=$2
+LOCK_FILE="$REPO_ADDR/lock_repo"
 
 echo "*** test update repos ***** "
 echo "SOURCE RPM Dir : $RPM_DIR"
 echo "destination repo : $REPO_ADDR"
+echo "Locker : $LOCK_FILE"
 
 echo "*** list RPMS ***"
 ls -l $RPM_DIR/*.rpm
 
-echo " *** copy RPMS ***"
-cp $RPM_DIR/*.rpm $REPO_ADDR
-
-echo "*** renew repo $REPO_ADDR ***"
-createrepo $REPO_ADDR
+write_lock $LOCK_FILE "update_repo_func"
 
 echo "*** clean everything ***"
-rm -rf $RPM_DIR
+#rm -f $RPM_DIR/*.rpm
