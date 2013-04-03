5425de06b3b9e37dae5bc84880e83e2e137f68b6
Adrien Devresse <Adrien.Devresse@cern.ch>
2013-04-03 14:13:46 +0200

    - fix mock build with a resilient mock directory locking

--- bin/mockbuild.sh
+++ bin/mockbuild.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+
+
+
 if [[ "$1" == "" || "$2" == "" || "$3" == "" ]]; then
 	echo "Usage $0 [config name] [srpm directory] [res_dir]"
 	exit 1
 fi
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR=$(dirname $(readlink -f $0) )
+
+# import copyfile
+source $DIR/lockers.sh
+
 
 echo "*** begin mock build *** "
 MOCK_CONFIG=$1
@@ -15,6 +22,7 @@ MOCK_CONFIG_DIRNAME="$( cd "$DIR/../mock_configs" && pwd )"
 SRC_RPM_DIR=$2
 MOCK_RESULT_DIR="/var/lib/mock/${MOCK_CONFIG}/result"
 DEST_RPM_DIR=$( echo "$3" | sed 's@\([^/]\)/*@\1@g')
+LOCK_FILE="/tmp/${MOCK_CONFIG}_lock"
 
 RPMS_NAME="`pwd`/${SRC_RPM_DIR}/`ls -t ${SRC_RPM_DIR} | head -n 1`"
 
@@ -26,11 +34,16 @@ ls $MOCK_CONFIG_DIRNAME
 
 echo "## dest dir ${DEST_RPM_DIR}/ "
 
+create_lock ${LOCK_FILE} 900
+
 echo "## launch mock clean ..."
 $MOCK_EXE --configdir=${MOCK_CONFIG_DIRNAME}/ -r $MOCK_CONFIG $MOCK_OPTS_CLEAN  || true
 echo "## launch mock ..."
 echo " build command mock --configdir=${MOCK_CONFIG_DIRNAME}/ -r $MOCK_CONFIG $RPMS_NAME"
 $MOCK_EXE --configdir=${MOCK_CONFIG_DIRNAME}/ -r $MOCK_CONFIG $RPMS_NAME
+
+delete_lock ${LOCK_FILE}
+
 
 echo "## creat link to result "
 rm -f $DEST_RPM_DIR
