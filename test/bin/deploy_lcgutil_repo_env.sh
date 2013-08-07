37b5d50e0acd4df6e37e3c9570b1324a6c4877b2
Adrien Devresse <Adrien.Devresse@cern.ch>
2013-08-07 11:00:20 +0200

    - update test deployment script

--- test/bin/deploy_lcgutil_repo_env.sh
+++ test/bin/deploy_lcgutil_repo_env.sh
@@ -4,10 +4,12 @@ MY_DIR="$(dirname $0)"
 MY_ABS_DIR="$(readlink -e ${MY_DIR})"
 
 
-PLATFORM="$(sed 's/.*\(el[0-9]\).*/\1/g')"
+PLATFORM="$( uname -a | sed 's/.*\(el[0-9]\).*/\1/g')"
 REPO_FILE="http://grid-deployment.web.cern.ch/grid-deployment/dms/lcgutil/repos/lcgutil-continuous-${PLATFORM}.repo"
 
 echo " launch setup lcgutil client env for $PLATFORM *** "
+
+set -e
 
 $MY_ABS_DIR/../lib/deploy_repo_file.sh $REPO_FILE
 $MY_ABS_DIR/../lib/deploy_voms_grid.sh
--- test/lib/deploy_repo_file.sh
+++ test/lib/deploy_repo_file.sh
@@ -6,7 +6,7 @@ export REPO_URL=$1
 echo " Deploy repo file $REPO_URL ..."
 
 pushd /etc/yum.repos.d/
-wget $REPO_URL
+wget $REPO_URL || { echo "Fetch repo file failure  !, Fatal !"; exit 1; }
 popd
 
 echo " List repos files $(ls /etc/yum.repos.d/) "
