ac2e351222e3d15ee5de5e9c5a47268ad643d2ad
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2014-05-07 08:35:14 +0200

    Updated rm regexp

--- bin/update_repos_rpm.sh
+++ bin/update_repos_rpm.sh
@@ -20,7 +20,7 @@ function clean_old_rpms {
     pushd $RPM_DIR
     for rpm in `ls -1 *.rpm`; do
         echo "   + " $rpm
-        rm_pattern=`echo $rpm | awk 'match($0, /^((([a-zA-Z]+-?)+)(([0-9]+\.)*([0-9]+)))/, m) { print m[1]"-*.rpm"; }'`
+        rm_pattern=`echo $rpm | awk 'match($0, /^((([a-zA-Z0-9]+-?)+)(([0-9]+\.)*([0-9]+)))/, m) { print m[1]"-*.rpm"; }'`
         echo "$REPO_ADDR/$rm_pattern"
         rm -vf $REPO_ADDR/$rm_pattern
     done
