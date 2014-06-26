d90326d5716141b169b6070933b62f5579e8f598
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2014-06-26 16:40:23 +0200

    Revert "Disabled temporarely rfc proxies"
    This reverts commit 26150d77fbbb3914ab8103f71312748c783bb5f9.

--- test/lib/generate_proxy_voms.sh
+++ test/lib/generate_proxy_voms.sh
@@ -15,8 +15,7 @@ export X509_USER_KEY="$CERTDIR/userkey.pem"
 
 
 echo "## Generate proxy certificate with $X509_USER_CERT and $X509_USER_KEY"
-# When RFC bug is fixed (stack smashing), enable it again!
-voms-proxy-init --debug --voms $MY_VO <<EOF
+voms-proxy-init --debug --rfc --voms $MY_VO <<EOF
 $VOMS_PASS
 EOF
 
